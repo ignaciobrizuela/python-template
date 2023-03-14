daa144231c31dbf7337998e6b3f12f74c67f057b
ibrizuela-eb <ibrizuela@eventbrite.com>
2023-03-14 13:00:23 -0300

    Initial configuration with tests

---
+++ src/app.py
@@ -0,0 +1,6 @@
+def say_hello_message():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    say_hello_message()
---
+++ src/tests/test_app.py
@@ -0,0 +1,5 @@
+from src.app import say_hello_message
+
+
+def test_foo():
+    assert "Hello World!" == say_hello_message()
